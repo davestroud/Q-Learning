84a806e3e536de516c85f9c647cfefbab08977a2
dstroud <david@davidstroud.me>
2020-10-10 12:00:43 -0500

    new notes for epsilon

--- RL_Santerre.py
+++ RL_Santerre.py
@@ -6,10 +6,17 @@ from keras.optimizers import Adam
 
 """ 
 Q-Learning can only work if the exploration policy explores
-the MDP thoroughly enough. We use the e-greedy policy, e is epsilon:
-at each step to act randomly with probability. 
+the MDP thoroughly enough. We use the e-greedy policy, e is 
+epsilon:at each step to act randomly with probability. 
+
+The advantage of the e-greedy plicy is that is will spend more
+and more time exploring interesting parts of the environment, 
+as the Q-Value estimates get better and better, while still 
+spending some time visiting unknown regions of the MDP. It is
+common to start with a high value for e (e.g.,1.0) and then
+gradually reduce it down to 0.05.
 """
-epsilon = .1
+epsilon = .1 # max(1-episode / 500, 0.01)
 epochs = 10000
 hidden_size = 50
 batch_size = 100
