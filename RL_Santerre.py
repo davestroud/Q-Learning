082a30222a698bfb499f08865a2b0dba84c05557
dstroud <david@davidstroud.me>
2020-10-10 11:51:10 -0500

    add santerre stroud file

--- RL_Santerre.py
+++ RL_Santerre.py
@@ -4,6 +4,11 @@ from keras.layers.core import Dense
 from keras.optimizers import Adam
 
 
+""" 
+Q-Learning can only work if the exploration policy explores
+the MDP thoroughly enough. We use the e-greedy policy, e is epsilon:
+at each step to act randomly with probability. 
+"""
 epsilon = .1
 epochs = 10000
 hidden_size = 50
@@ -21,3 +26,6 @@ model.add(Dense(hidden_size, input_shape=(len(target),), activation='relu'))
 model.add(Dense(hidden_size, activation='relu'))
 model.add(Dense(num_targets, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001))
+
+def guess_number(model, guess_vec):
+    if np.random.rand() <= epsilon
